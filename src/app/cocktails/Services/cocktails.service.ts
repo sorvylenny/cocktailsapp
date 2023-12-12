a74ec1a2f8cd166e186e5bbf98998dead2b1dac5
Tu Nombre <115513656+sorvylenny@users.noreply.github.com>
2023-12-11 22:52:48 -0500

    set search and arrange the css
    set search and arrange the css in the button, the card init and card id

--- src/app/cocktails/Services/cocktails.service.ts
+++ src/app/cocktails/Services/cocktails.service.ts
@@ -14,6 +14,10 @@ private baseUrl: string= environment.baseUrl;
 
   constructor( private http: HttpClient) { }
 
+  searchCocktailByName(name: string): Observable<any> {
+    const url = `${this.baseUrl}/search.php?s=${name}`;
+    return this.http.get(url);
+  }
   getCocktailsByGlass(glassType: string): Observable<Cocktails> {
     const url = `${this.baseUrl}/filter.php?g=${glassType}`;
     return this.http.get<Cocktails>(url);
--- src/app/cocktails/pages/list-cocktails/list-cocktails.component.ts
+++ src/app/cocktails/pages/list-cocktails/list-cocktails.component.ts
@@ -12,7 +12,11 @@ export class ListCocktailsComponent implements OnInit {
 
   cocktailGlassCocktails: Drink[] = [];
   ordinaryDrinkCocktails: Drink[] = [];
-  
+  dataCocktails: any[] = [];
+  searchTerm: string = '';
+  activeTabIndex: number=0;
+  noResultsMessage: string = '';
+
 
   constructor(private cocktailService: CocktailsService, private router: Router) {}
 
@@ -53,4 +57,67 @@ export class ListCocktailsComponent implements OnInit {
       }
     );
   }
+
+  appSearchTable(inputValue: string) {
+    const searchTerm = inputValue.trim().toLowerCase();
+    console.log('Término de búsqueda:', searchTerm);
+
+    if (!searchTerm) {
+      this.showNoResultsMessage();
+      this.getCocktailsByCategory('Ordinary_Drinks');
+      this.getCocktailsByGlass('Cocktail_glass');
+      return;
+    }
+
+    this.cocktailService.searchCocktailByName(searchTerm).subscribe(
+      (data) => {
+        console.log('Datos de la API:', data);
+
+        if (!data || !data.drinks) {
+          console.error('La respuesta de la API es nula o no contiene la propiedad "drinks".');
+          // Mostrar mensaje de no hay resultados o ejecutar otras funciones si es necesario
+          this.showNoResultsMessage();
+          this.getCocktailsByCategory('Ordinary_Drinks');
+          this.getCocktailsByGlass('Cocktail_glass');
+          return;
+        }
+
+        // Filtra las bebidas por el nombre
+        const searchResults = (data.drinks || []).filter((drink: any) => {
+          const drinkName = (drink.strDrink || '').trim().toLowerCase();
+          console.log('Drink Name:', drinkName);
+          return drinkName.includes(searchTerm);
+        });
+
+        // Muestra los resultados en la pestaña activa actual
+        switch (this.activeTabIndex) {
+          case 0:
+            this.cocktailGlassCocktails = searchResults;
+            break;
+          case 1:
+            this.ordinaryDrinkCocktails = searchResults;
+            break;
+          // Agrega más casos según sea necesario para otras pestañas
+        }
+
+        // Imprime los resultados
+        console.log('Resultados:', searchResults);
+      },
+      (error) => {
+        console.error('Error fetching cocktails:', error);
+      }
+    );
+  }
+
+  // Otras funciones necesarias, como getCocktailsByCategory y getCocktailsByGlass
+
+  tabChanged(tabIndex: number) {
+    this.activeTabIndex = tabIndex;
+  }
+  showNoResultsMessage() {
+    this.searchTerm = 'No se encontraron resultados o no se proporcionó un término de búsqueda.';
+  }
+
+
+
 }
